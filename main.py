1694b46e782b423f38d719b9324ba9c7c5c42a7e
Simon Wibberley <simon.wibberley@gmail.com>
2020-09-01 15:59:24 +0100

    updated date gen code, added sitemap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+from flask import Response
 import random
 import lorem
 import dominate
@@ -7,26 +9,58 @@ from dominate.tags import *
 
 app = Flask(__name__)
 
+
+link_min=0
+link_max=50
+keyword_prob=0.6
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def hello_world(path):
+    host = request.headers.get("Host")
+    print(host)
     if path == "robots.txt":
         return ""
-    has_keyword = random.random() < 0.1
-    return gen_page(has_keyword)
+    if path.startswith("sitemap"):
+        return sitemap(host)
+    has_keyword = random.random() < keyword_prob
+
+    page = gen_page(host, path, has_keyword)
+    return Response(page, mimetype='text/html')
 
 
-def gen_page(has_keyword):
+def sitemap(host):
+    sitemap = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+   <url>
+      <loc>http://%s/123.html</loc>
+      <lastmod>2005-01-01</lastmod>
+      <changefreq>monthly</changefreq>
+      <priority>0.8</priority>
+   </url>
+</urlset> """ % host
+    return sitemap
+    
+def gen_page(host, path, has_keyword):
     doc = dominate.document(title="fakenet")
+
+    #with doc.head:
+    #    link(rel="canonical", href="http://"+host+"/real_"+path)
+
     with doc:
         with div(id='menu').add(ul(id='links')):
-            for link in gen_links():
-                li(a(link, href=link))
+            for l in gen_links():
+                li(a(l, href=l))
 
         with div(id='content'):
             with div():
+
                 attr(cls='article')
-                span(datetime.datetime.now().date().isoformat(), cls='date')
+                delta = random.randint(0, 10) * -1
+                now = datetime.datetime.now().date()
+                d = now + datetime.timedelta(delta)
+
+                span(d.isoformat(), cls='date')
                 if has_keyword:
                     p("attack")
                 [p(lorem.paragraph()) for i in range(10)]
@@ -36,8 +70,9 @@ def gen_page(has_keyword):
 
 def gen_links():
     links = []
-    for i in range(random.randint(0,100)):
-        links.append("/%d.html" % i)
+    for i in range(random.randint(link_min,link_max)):
+        l = random.randint(0, 1000)
+        links.append("/%d.html" % l)
     return links
 
 
